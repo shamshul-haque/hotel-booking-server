c39b0545b84f7a31a41512bdcedd119ff8cd2960
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-11-06 17:35:55 +0600

    create database and connect server with mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
+
+// parsers
+app.use(
+  cors({
+    origin: ["http://localhost:5173/"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// connecting uri
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DD_Pass}@cluster0.gwehrjf.mongodb.net/?retryWrites=true&w=majority`;
+
+// db connection
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
