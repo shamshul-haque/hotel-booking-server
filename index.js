b5777107f32fdff2d46ae2189f53bd23d82dfac6
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-11-07 01:53:38 +0600

    fixed cross origin bug

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // parsers
 app.use(
   cors({
-    origin: ["http://localhost:5173/"],
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
